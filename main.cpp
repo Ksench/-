8af6bb6582abcce714f08492598067d555b47271
Kovaleva Ksenya <Ksu@mail.ru>
2015-10-14 15:33:52 +0300

    реализовала алгоритм половинного деления

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+
+double f(double x)
+{
+    const double alpha = 0.1;
+    return sin(x) - alpha*x;
+}
+
+double dichotomy (
+       double x1,
+       double x2,
+       double tolerance
+
+        // , TODO: pass f(x)
+        )
+
+{
+
+    while (fabs(x1-x2) > tolerance)
+    {
+       double x3 = (x1+x2)/2;
+
+        if (f(x3)*f(x2) < 0)
+        x1 = x3;
+        else x2 = x3;
+
+  }
+    return x2;
+
+}
+
 int main(int argc, char *argv[])
 {
-    int mas[10];
-    for (int i=0; i<10; i++)
-    cout << mas[i] << endl;
 
-    //cout << "hello" << endl;
-       return 0;
+ double pi = 2*asin(1);
+ cout << dichotomy(pi/2 , 3*pi/2 , 0.001) << endl;
+ return 0;
 }
